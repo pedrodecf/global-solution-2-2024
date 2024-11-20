60d34195be37f48c2f34f901de61054477ecb920
@pedrodecf <contato.pedrodecf@gmail.com>
2024-11-20 11:50:13 -0300

    navbar estilizada

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,8 +2,32 @@ import logo from "../../assets/logo.svg";
 
 export function Navbar() {
   return (
-    <header className="w-full py-4 flex flex-row justify-between mb-6">
+    <header className="w-full py-4 flex flex-row justify-between mb-6 items-center">
       <img src={logo} alt="Sustergy" className="w-40 h-auto" />
+      <nav>
+        <ul className="flex flex-row gap-8">
+          <li>
+            <a href="#quem-somos" className="text-sm text-gray hover-nav">
+              Quem somos
+            </a>
+          </li>
+          <li>
+            <a href="#como-funciona" className="text-sm text-gray hover-nav">
+              Como funciona
+            </a>
+          </li>
+          <li>
+            <a href="#contato" className="text-sm text-gray hover-nav">
+              Preços
+            </a>
+          </li>
+          <li>
+            <a href="#contato" className="text-sm text-gray hover-nav">
+              Contato
+            </a>
+          </li>
+        </ul>
+      </nav>
 
       <button
         className="bg-gradient-to-r from-[#62E763] to-[#3CC889]
