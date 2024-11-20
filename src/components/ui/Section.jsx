f08e3b462794f259dca5126d70447368c4d3a5cc
@pedrodecf <contato.pedrodecf@gmail.com>
2024-11-20 11:57:41 -0300

    adicionando tailwind merge

--- src/components/ui/Section.jsx
+++ src/components/ui/Section.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 function Section({ children, className = "" }) {
   return (
     <section
-      className={`max-w-[1280px] mx-auto px-8 flex flex-col items-center ${className}`}
+      className={twMerge(
+        "max-w-[1280px] mx-auto px-8 flex flex-col items-center",
+        className
+      )}
     >
       {children}
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export function Home() {
     <>
       <Hero />
       <Background className="bg-black">
-        <Section className="bg-dark-green px-0 relative">
+        <Section className="bg-dark-green relative px-0">
           <div className="z-20 absolute w-full flex flex-row items-center px-8 mt-6 gap-2">
             <h2 className=" text-gray bg-transparent text-sm">
               Em parceria com as mais diversas empresas
